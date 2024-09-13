3cc748504cf68bdc7ee380484ab95ca4024d0b3d
LukSullock <luksullockenzlin@hotmail.com>
2024-09-14 00:38:13 +0200

    Licence update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 24 18:36:43 2024
+SpikeAnalysis tool. A tool to analyse neuronal spike activity.
 
-@author: LukSu
+Copyright (C) 2024 Luk Sullock Enzlin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from modules.GUI.GUIMain import start as Gui
 
@@ -10,13 +23,10 @@ from modules.GUI.GUIMain import start as Gui
 if __name__=="__main__":
     Gui()
 
-
-
 """To do
 >Burst analyse
 >Threshold plotting
 
->Github
 >Github wiki
 >Github handleiding
 >Programma handleiding
